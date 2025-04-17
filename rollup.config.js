a574963f377848299e6ed7c9cb877aa782aea387
Nicholas Simpkins <nsimpkins@hawk.iit.edu>
2025-04-17 18:26:42 -0500

    Added starter template files + code

---
+++ rollup.config.js
@@ -0,0 +1,72 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import babel from '@rollup/plugin-babel';
+import terser from '@rollup/plugin-terser';
+import pkg from './package.json';
+
+export default [
+  // UMD build (for browsers)
+  {
+    input: 'src/index.js',
+    output: {
+      name: 'VisionCharts',
+      file: 'dist/visioncharts.js',
+      format: 'umd',
+      sourcemap: true,
+    },
+    plugins: [
+      resolve(),
+      commonjs(),
+      babel({
+        babelHelpers: 'bundled',
+        exclude: 'node_modules/**',
+        presets: [
+          ['@babel/preset-env', { targets: '> 0.25%, not dead' }]
+        ]
+      })
+    ]
+  },
+  
+  // Minified UMD build (for browsers)
+  {
+    input: 'src/index.js',
+    output: {
+      name: 'VisionCharts',
+      file: 'dist/visioncharts.min.js',
+      format: 'umd',
+      sourcemap: true,
+    },
+    plugins: [
+      resolve(),
+      commonjs(),
+      babel({
+        babelHelpers: 'bundled',
+        exclude: 'node_modules/**',
+        presets: [
+          ['@babel/preset-env', { targets: '> 0.25%, not dead' }]
+        ]
+      }),
+      terser()
+    ]
+  },
+  
+  // ESM build (for bundlers)
+  {
+    input: 'src/index.js',
+    output: {
+      file: pkg.module,
+      format: 'es',
+      sourcemap: true,
+    },
+    plugins: [
+      resolve(),
+      babel({
+        babelHelpers: 'bundled',
+        exclude: 'node_modules/**',
+        presets: [
+          ['@babel/preset-env', { targets: { node: 14 } }]
+        ]
+      })
+    ]
+  }
+];
